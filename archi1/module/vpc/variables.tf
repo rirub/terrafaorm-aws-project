aee18dbf2981746aa336b81924700507b459006c
eunyoung <risilze@naver.com>
2023-06-14 18:53:56 +0900

    alb&asg&rds&variable 주석해제

--- archi1/module/vpc/variables.tf
+++ archi1/module/vpc/variables.tf
@@ -1,29 +1,29 @@
-# variable "tags" {
-#   type    = string
-#   default="key"
-# }
+variable "tags" {
+  type    = string
+  default="key"
+}
 
-# variable "region" {
-#   type    = string
-#   default = "ap-northeast-2"
-# }
+variable "region" {
+  type    = string
+  default = "ap-northeast-2"
+}
 
-# variable "networking" {
-#   type = object({
-#     cidr_block      = string
-#     region          = string
-#     vpc_name        = string
-#     azs             = list(string)
-#     public_subnets  = list(string)
-#     private_subnets = list(string)
-#   })
-#   default = {
-#     cidr_block      = "10.0.0.0/16"
-#     region          = "ap-northeast-2"
-#     vpc_name        = "custom-vpc"
-#     azs             = ["ap-northeast-2a", "ap-northeast-2c"]
-#     public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-#     private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
-#   }
-#  }
+variable "networking" {
+  type = object({
+    cidr_block      = string
+    region          = string
+    vpc_name        = string
+    azs             = list(string)
+    public_subnets  = list(string)
+    private_subnets = list(string)
+  })
+  default = {
+    cidr_block      = "10.0.0.0/16"
+    region          = "ap-northeast-2"
+    vpc_name        = "custom-vpc"
+    azs             = ["ap-northeast-2a", "ap-northeast-2c"]
+    public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+    private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  }
+ }
 
